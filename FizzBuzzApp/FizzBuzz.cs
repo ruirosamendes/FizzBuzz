39c2020ec26afa3fae6bd14fae3345a4c321e8b0
Rui Rosa Mendes <ruirosamendes@gmail.com>
2015-08-12 00:54:38 +0100

    Fizzbuzz Step3 -Do Words Count Report

--- FizzBuzzApp/FizzBuzz.cs
+++ FizzBuzzApp/FizzBuzz.cs
@@ -1,16 +1,27 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace FizzBuzzApp
 {
     public class FizzBuzz
-    {      
-        internal string Output(int bottomNumber, int topNumber)
+    {
+        Dictionary<string, int> _wordsCount = new Dictionary<string, int>();
+
+        private void WordCount(string word)
+        {
+            if (!_wordsCount.ContainsKey(word))
+                _wordsCount.Add(word, 1);
+            else
+                _wordsCount[word]++;
+        }
+
+        internal string LineOutput(int bottomNumber, int topNumber)
         {
             StringBuilder lineOutput = new StringBuilder();
             string numberOutput = string.Empty;
-
+            
             try
             {
                 for (int number = bottomNumber; number <= topNumber; number++)
@@ -30,6 +41,12 @@ namespace FizzBuzzApp
                         lineOutput.Append(numberOutput);
                     else
                         lineOutput.Append(numberOutput + " ");
+
+                    int numberInt;
+                    if (int.TryParse(numberOutput, out numberInt))
+                        WordCount("integer");
+                    else
+                        WordCount(numberOutput);                                            
                 }
             }
             catch (OutOfMemoryException)
@@ -39,11 +56,25 @@ namespace FizzBuzzApp
             }
             return lineOutput.ToString();
         }
+      
+        internal string ReportOutput(int bottomNumber, int topNumber)
+        {
+            StringBuilder reportOutput = new StringBuilder();
+
+            reportOutput.Append(LineOutput(bottomNumber, topNumber) + Environment.NewLine);
+            reportOutput.Append("fizz: " + _wordsCount["fizz"] + Environment.NewLine);
+            reportOutput.Append("buzz: " + _wordsCount["buzz"] + Environment.NewLine);
+            reportOutput.Append("fizzbuzz: " + _wordsCount["fizzbuzz"] + Environment.NewLine);
+            reportOutput.Append("lucky: " + _wordsCount["lucky"] + Environment.NewLine);
+            reportOutput.Append("integer: " + _wordsCount["integer"]);
+
+            return reportOutput.ToString();
+        }
 
         public void WriteOutput(int bottomNumber, int topNumber)
         {
-            Console.WriteLine(Output(bottomNumber, topNumber));
-        }
+            Console.Write(ReportOutput(bottomNumber, topNumber));
+        }        
     }
 
     [TestFixture()]
@@ -55,18 +86,18 @@ namespace FizzBuzzApp
         {
             string expectedOutput = @"1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz";
             FizzBuzz fizzBuzz = new FizzBuzz();            
-            Assert.AreEqual(expectedOutput, fizzBuzz.Output(1, 20));
+            Assert.AreEqual(expectedOutput, fizzBuzz.LineOutput(1, 20));
         }
 
         [Test()]
         public void FizzBuzzBottomHigherThanTopOutputEmptyString()
         {
             FizzBuzz fizzBuzz = new FizzBuzz();
-            Assert.AreEqual(string.Empty, fizzBuzz.Output(20, 1));
+            Assert.AreEqual(string.Empty, fizzBuzz.LineOutput(20, 1));
         }
 
-        [Test()]
-        public void FizzBuzzBottomHandlesOutOfMemoryException()
+        [Test()]        
+        public void FizzBuzzHandlesOutOfMemoryException()
         {
             FizzBuzz fizzBuzz = new FizzBuzz();
             Assert.Catch< OutOfMemoryException>(
@@ -74,7 +105,7 @@ namespace FizzBuzzApp
                 {
                        Assert.AreEqual (
                          "Huge contiguous range. Please select smaller contiguous range.",
-                        fizzBuzz.Output(1, int.MaxValue));
+                        fizzBuzz.LineOutput(1, int.MaxValue));
                 });            
         }
 
@@ -83,7 +114,21 @@ namespace FizzBuzzApp
         {
             string expectedOutput = @"1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz";
             FizzBuzz fizzBuzz = new FizzBuzz();
-            Assert.AreEqual(expectedOutput, fizzBuzz.Output(1, 20));
+            Assert.AreEqual(expectedOutput, fizzBuzz.LineOutput(1, 20));
+        }
+
+        [Test()]
+        public void FizzBuzzMustReportWordsCounts()
+        {
+            string expectedOutput = 
+                "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz" + Environment.NewLine +
+                "fizz: 4" + Environment.NewLine +
+                "buzz: 3" + Environment.NewLine +
+                "fizzbuzz: 1" + Environment.NewLine +
+                "lucky: 2" + Environment.NewLine +
+                "integer: 10";
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            Assert.AreEqual(expectedOutput, fizzBuzz.ReportOutput(1, 20));
         }
 
     }
